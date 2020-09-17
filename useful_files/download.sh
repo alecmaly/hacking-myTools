048c375b9e73fb48c4afc8ff8e7a254c8c7a5452
alec <alecjmaly@gmail.com>
2020-09-17 13:44:48 -0400

    updating

--- useful_files/download.sh
+++ useful_files/download.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-cat /opt/myTools/useful_files/util/_files_to_download_linux.txt | xargs wget -N -P /opt/myTools/useful_files/files/linux
-cat /opt/myTools/useful_files/util/_files_to_download_windows.txt | xargs wget -N -P /opt/myTools/useful_files/files/windows
+# cat /opt/myTools/useful_files/util/_files_to_download_linux.txt | xargs wget -N -P /opt/myTools/useful_files/files/linux
 
+
+# cat /opt/myTools/useful_files/util/_files_to_download_windows.txt | xargs wget -N -P /opt/myTools/useful_files/files/windows
+
+mkdir cd /opt/myTools/useful_files/files/windows/github 2>/dev/null
+cat /opt/myTools/useful_files/util/_github_windows.txt | xargs -I{} sh -c "cd /opt/myTools/useful_files/files/windows/github; git clone {}"
