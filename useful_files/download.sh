e3b0ad71f71063000de9aa04e8c0eed6effa7c84
alec <alecjmaly@gmail.com>
2020-09-17 13:48:50 -0400

    updating

--- useful_files/download.sh
+++ useful_files/download.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# cat /opt/myTools/useful_files/util/_files_to_download_linux.txt | xargs wget -N -P /opt/myTools/useful_files/files/linux
+##  download linux
+# files
+cat /opt/myTools/useful_files/util/_files_to_download_linux.txt | xargs wget -N -P /opt/myTools/useful_files/files/linux
+# mkdir cd /opt/myTools/useful_files/files/linux/github 2>/dev/null
+# cat /opt/myTools/useful_files/util/_github_linux.txt | xargs -I{} sh -c "cd /opt/myTools/useful_files/files/linux/github; git clone {}"
 
 
-# cat /opt/myTools/useful_files/util/_files_to_download_windows.txt | xargs wget -N -P /opt/myTools/useful_files/files/windows
-
+##  download windows
+# files
+cat /opt/myTools/useful_files/util/_files_to_download_windows.txt | xargs wget -N -P /opt/myTools/useful_files/files/windows
+# github
 mkdir cd /opt/myTools/useful_files/files/windows/github 2>/dev/null
 cat /opt/myTools/useful_files/util/_github_windows.txt | xargs -I{} sh -c "cd /opt/myTools/useful_files/files/windows/github; git clone {}"
+
+
+
+##  download all
+# github
+mkdir cd /opt/myTools/useful_files/files/all/github 2>/dev/null
+cat /opt/myTools/useful_files/util/_github_all.txt | xargs -I{} sh -c "cd /opt/myTools/useful_files/files/all/github; git clone {}"
