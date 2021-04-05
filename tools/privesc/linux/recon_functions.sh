192ab57b64c7b90d81c6f0f3bba6c67092673e86
root <alecjmaly@gmail.com>
2021-04-05 12:24:36 -0400

    udpated

--- tools/privesc/linux/recon_functions.sh
+++ tools/privesc/linux/recon_functions.sh
@@ -168,9 +168,13 @@ mgrep -sr "[sS][qQ][lL]" -n 10 -mr '"'"'0.0.0.0.*\n.*\n\|localhost.*\n.*\n\|127.
 ## grep passwords from current directory
 # greppass
 
+function grepdb {
+  ## grepdb: sql followed by host
+  mgrep -sr "[sS][qQ][lL]" -n 10 -mr '0.0.0.0.*\n.*\n.*\|localhost.*\n.*\n.*\|127.0.0.1.*\n.*\n.*' -gc 'sql\|localhost\|127.0.0.1\|0.0.0.0' -xd "node_modules" # -xh
+}
 
-## grepdb: sql followed by host
-# mgrep -sr "[sS][qQ][lL]" -n 10 -mr '0.0.0.0.*\n.*\n.*\|localhost.*\n.*\n.*\|127.0.0.1.*\n.*\n.*' -gc 'sql\|localhost\|127.0.0.1\|0.0.0.0' -xd "node_modules" # -xh
 
-## grepdb: host followed by sql
-# mgrep -sr '0.0.0.0\|localhost\|127.0.0.1' -n 10 -mr "[sS][qQ][lL]" -gc 'qsql\|localhost\|127.0.0.1\|0.0.0.0' -xd "node_modules" # -xh
+function grepdbr {
+  ## grepdb: host followed by sql
+  mgrep -sr '0.0.0.0\|localhost\|127.0.0.1' -n 10 -mr "[sS][qQ][lL]" -gc 'qsql\|localhost\|127.0.0.1\|0.0.0.0' -xd "node_modules" # -xh
+}
