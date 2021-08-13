1af10e6f76f2cdfab170f903d262690f1d1f5eaa
alecjmaly <alecjmaly@gmail.com>
2021-08-13 10:23:18 -0400

    updated burp configs

--- tools/upgradeburpsuite.sh
+++ tools/upgradeburpsuite.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-latest_url="$(curl https://portswigger.net/burp/releases/community/latest -s | grep 'burp'|awk -F'href="' '{print $2}'|cut -d '?' -f1)"
+latest_url="$(curl https://portswigger.net/burp/releases/community/latest -s -L | grep 'burp'|awk -F'href="' '{print $2}'|cut -d '?' -f1 | grep releases | grep community | sed 's#"/>##g' | tr -d '\n' | tr -d '\r')"
 download_url="http://portswigger.net$(curl $latest_url -s |grep "jar"|grep download|grep community|awk -F 'href=' '{print $2}'|cut -d' ' -f1|sed 's/amp;//g')"
 
 wget $download_url -O burpsuite
