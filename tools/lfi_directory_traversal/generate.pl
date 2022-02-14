906ec9e4e7e75f22dbb092436b58d8f7abfbd00e
alecjmaly <alecjmaly@gmail.com>
2022-02-14 10:01:06 -0500

    updated

--- tools/lfi_directory_traversal/generate.pl
+++ tools/lfi_directory_traversal/generate.pl
@@ -14,11 +14,9 @@ open(FH, '>', "lfi_directory_traversal_wordlist.txt") or die $!;
 
 $counter = 0;
 foreach $file (@files) {
-    print $file . "\n";
+    print FH $file . "\n"; # output to file
     @traversals = TraversalEngine($OS, $min_deep, $deep, $file);
     foreach $trav (@traversals) {
-        print $trav . "\n";
-        
         print FH $trav . "\n"; # output to file
         $counter = $counter + 1;
     }
