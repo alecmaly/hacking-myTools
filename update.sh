83e5d5cfeba5fa6eb4047ee040bc3c4329703115
alecjmaly <alecjmaly@gmail.com>
2022-03-29 21:21:34 -0400

    updated

--- update.sh
+++ update.sh
@@ -1,8 +1,6 @@
 
 
 
-
-
 # build wordlists
 echo [+] updating seclists
 cd /opt/SecLists
@@ -20,10 +18,11 @@ wlk /opt/SecLists/Discovery/SNMP/common-snmp-community-strings.txt /opt/SecLists
 echo [+] building largeRaftPlus2.3Medium-lowercase.txt wordlist
 wlk /opt/hacking-myTools/wordlists/custom-base/directory-brute.txt /opt/SecLists/Discovery/Web-Content/raft-large-words-lowercase.txt /opt/SecLists/Discovery/Web-Content/common.txt /opt/SecLists/Discovery/Web-Content/directory-list-lowercase-2.3-medium.txt > /opt/hacking-myTools/wordlists/largeRaftPlus2.3Medium-lowercase.txt
 
-
 # password wordlist
 echo [+] building fasttrack common-creds darkweb-top10000 wordlist
-wlk /usr/share/wordlists/fasttrack.txt /opt/SecLists/Passwords/Common-Credentials/best1050.txt /opt/SecLists/Passwords/darkweb2017-top10000.txt > /opt/hacking-myTools/wordlists/top1000_1050_fasttrack.txt
+# download fasttrack.txt
+wget -q "https://raw.githubusercontent.com/drtychai/wordlists/master/fasttrack.txt" -O /usr/share/wordlists/fasttrack_backup.txt
+wlk /usr/share/wordlists/fasttrack_backup.txt /opt/SecLists/Passwords/Common-Credentials/best1050.txt /opt/SecLists/Passwords/darkweb2017-top10000.txt > /opt/hacking-myTools/wordlists/top1000_1050_fasttrack.txt
 
 
 
