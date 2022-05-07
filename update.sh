21a9066255e62806840e67fc7cf642365c73bd82
root <alecjmaly@gmail.com>
2022-05-06 22:48:33 -0400

    added fuzz.txt wordlist

--- update.sh
+++ update.sh
@@ -22,7 +22,9 @@ wlk /opt/hacking-myTools/wordlists/custom-base/directory-brute.txt /opt/SecLists
 echo [+] building fasttrack common-creds darkweb-top10000 wordlist
 # download fasttrack.txt
 wget -q "https://raw.githubusercontent.com/drtychai/wordlists/master/fasttrack.txt" -O /usr/share/wordlists/fasttrack_backup.txt
-wlk /usr/share/wordlists/fasttrack_backup.txt /opt/SecLists/Passwords/Common-Credentials/best1050.txt /opt/SecLists/Passwords/darkweb2017-top10000.txt > /opt/hacking-myTools/wordlists/top1000_1050_fasttrack.txt
+# download fuzz.txt
+wget -q "https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt" -O /usr/share/wordlists/fuzz.txt
+wlk /usr/share/wordlists/fasttrack_backup.txt /usr/share/wordlists/fuzz.txt /opt/SecLists/Passwords/Common-Credentials/best1050.txt /opt/SecLists/Passwords/darkweb2017-top10000.txt > /opt/hacking-myTools/wordlists/top1000_1050_fasttrack.txt
 
 
 
